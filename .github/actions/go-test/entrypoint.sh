d9944f650d7dcb7c46fb4039630ae2754aa5b449
ocket8888 <ocket8888@gmail.com>
2019-12-19 09:11:15 -0700

    Added dependency fetch step

--- .github/actions/go-test/entrypoint.sh
+++ .github/actions/go-test/entrypoint.sh
@@ -13,4 +13,6 @@ mkdir -p "$SRCDIR"
 ln -s "$PWD" "$SRCDIR/trafficcontrol"
 cd "$SRCDIR/trafficcontrol"
 
+# Need to fetch golang.org/x/* dependencies
+/usr/local/go/bin/go get -v $INPUT_DIR
 /usr/local/go/bin/go test -v $INPUT_DIR
