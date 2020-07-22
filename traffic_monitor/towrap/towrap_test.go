5a80ef996a7c71113ca5f4a4800b2ed546575ba9
ocket8888 <ocket8888@apache.org>
2020-07-22 14:18:39 -0600

    Fix towrap tests testing functionality the package doesn't have
    The tests are already being performed in the place to which the
    aforementioned functionality has been moved.

--- traffic_monitor/towrap/towrap_test.go
+++ traffic_monitor/towrap/towrap_test.go
@@ -24,29 +24,3 @@ import (
 
 	"github.com/apache/trafficcontrol/lib/go-tc"
 )
-
-func TestMonitorConfigValid(t *testing.T) {
-	mc := (*tc.LegacyTrafficMonitorConfigMap)(nil)
-	if MonitorConfigValid(mc) == nil {
-		t.Errorf("MonitorCopnfigValid(nil) expected: error, actual: nil")
-	}
-	mc = &tc.LegacyTrafficMonitorConfigMap{}
-	if MonitorConfigValid(mc) == nil {
-		t.Errorf("MonitorConfigValid({}) expected: error, actual: nil")
-	}
-
-	validMC := &tc.LegacyTrafficMonitorConfigMap{
-		TrafficServer:   map[string]tc.LegacyTrafficServer{"a": {}},
-		CacheGroup:      map[string]tc.TMCacheGroup{"a": {}},
-		TrafficMonitor:  map[string]tc.TrafficMonitor{"a": {}},
-		DeliveryService: map[string]tc.TMDeliveryService{"a": {}},
-		Profile:         map[string]tc.TMProfile{"a": {}},
-		Config: map[string]interface{}{
-			"peers.polling.interval":  42.0,
-			"health.polling.interval": 24.0,
-		},
-	}
-	if err := MonitorConfigValid(validMC); err != nil {
-		t.Errorf("MonitorConfigValid(%++v) expected: nil, actual: %+v", validMC, err)
-	}
-}
